8d6549bec6211332b3893cf99de5d08d2dfa1f8e
yiqingzhao <yiqingzhao@tamu.edu>
2020-04-11 00:52:56 -0500

    add feature for validating user input

--- OrderedImages/js/sorting.js
+++ OrderedImages/js/sorting.js
@@ -1,3 +1,8 @@
+
+window.onload = function() {
+    this.jQuery(); 
+}
+
 jQuery(function(){
 
     // loop through the original items...
@@ -7,6 +12,7 @@ jQuery(function(){
         // absolute-positioned counterparts...
         var item = jQuery(this);
         var item_clone = item.clone();
+
         // 'store' the clone for later use...
         item.data("clone", item_clone);
 
@@ -17,8 +23,11 @@ jQuery(function(){
 
         // append the clone...
         jQuery("#cloned_items").append(item_clone);
-    });
+        // make sure all our original items are visible again...
+        item_clone.css("visibility", "hidden");
 
+    });
+    var init = false;
     // create our sortable as usual...
     // with some event handler extras...
     jQuery("#original_items").sortable({
@@ -27,6 +36,19 @@ jQuery(function(){
         // only adjust the visibility, we still need
         // their float positions..!
         start: function(e, ui){
+            if (!init) {
+                jQuery("#cloned_items li").each(function(){
+
+                    // clone the original items to make their
+                    // absolute-positioned counterparts...
+                    var item = jQuery(this);
+                    
+                    item.css("visibility", "visible");
+            
+                });
+                init = true;
+            }
+
             // loop through the items, except the one we're
             // currently dragging, and hide it...
             ui.helper.addClass("exclude-me");
@@ -56,6 +78,49 @@ jQuery(function(){
 
             // make sure all our original items are visible again...
             jQuery("#original_items li").css("visibility", "visible");
+
+            var counter = 0;
+            // loop through the original items after order modification
+            jQuery("#original_items li").each(function(){
+								// var item = jQuery(this);
+                // obtain position of the item
+                var nthItem = $(this).index();
+                var itemID = $(this).attr('id');
+                switch(nthItem) {
+                    case 0:
+                        if (itemID == "first") {
+                            ++counter;
+                        }
+                        break;
+                    case 1:
+                        if (itemID == "second") {
+                            ++counter;
+                        }
+                        break;
+                    case 2:
+                        if (itemID == "third") {
+                            ++counter;
+                        }
+                        break;
+                    case 3:
+                        if (itemID == "fourth") {
+                            ++counter;
+                        }
+                        break;
+                    case 4:
+                        if (itemID == "fifth") {
+                            ++counter;
+                        }
+                        break;
+                    default: 
+                        break;
+                }
+            });
+            if (counter == 5) {
+                $("#checked_order").html("<b style='color: green'>Correct Ordering! Good Job!</b>")
+            } else {
+                $("#checked_order").html("<b style='color: red'>Incorrect Ordering!</b>")
+            }
         },
 
         // here's where the magic happens...
@@ -79,4 +144,5 @@ jQuery(function(){
             });
         }
     });
+    $('#original_items').trigger('mousemove');
 });
